fa239d83b765322050c9961dbd4bc55a5d68a918
laboratorio <laboratorio@UPOAULA10702>
2025-07-10 07:17:28 -0500

    Agregar archivos de proyecto.

---
+++ AntambaJExamenFinalProgramacionTres/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AntambaJExamenFinalProgramacionTres
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

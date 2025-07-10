b329813fecd53b56c7e33943ae76ef055726fb7f
JuanAntamba <juan.antamba@udla.edu.ec>
2025-07-10 07:28:33 -0500

    Creacion Modelo Suscripcion

---
+++ AntambaJExamenFinalProgramacionTres/Models/Suscripcion.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntambaJExamenFinalProgramacionTres.Models
+{
+    public class Suscripcion
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [NotNull]
+        public string Servicio { get; set; }
+        [NotNull]
+        public string CorreoAsociado { get; set; }
+        [NotNull]
+        public decimal CostoMensual { get; set; }
+        public bool RenovacionAutomatica { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
+

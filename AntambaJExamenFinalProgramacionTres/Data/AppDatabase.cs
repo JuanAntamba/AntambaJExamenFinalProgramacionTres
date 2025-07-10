89bfd033e7fd3b3f023e4ac52faca3291ad13686
JuanAntamba <juan.antamba@udla.edu.ec>
2025-07-10 07:34:40 -0500

    Clase AppDatabae

---
+++ AntambaJExamenFinalProgramacionTres/Data/AppDatabase.cs
@@ -0,0 +1,30 @@
+﻿using AntambaJExamenFinalProgramacionTres.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntambaJExamenFinalProgramacionTres.Data
+{
+    public class AppDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public AppDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Suscripcion>().Wait();
+        }
+
+        public Task<int> InsertSuscripcionAsync(Suscripcion s) =>
+            _database.InsertAsync(s);
+
+        public Task<List<Suscripcion>> GetSuscripcionesAsync() =>
+            _database.Table<Suscripcion>().ToListAsync();
+    }
+}
+
+
+
